c0c236ed08abf684b0bfb9af98408dd494b3dc0b
ft4k696bk6-prog <ft4k696bk6@privaterelay.appleid.com>
2026-05-21 00:36:37 +0200

    Add GA4 Google tag

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { initGoogleAnalytics } from './lib/google-analytics'
 import './index.css'
 import App from './App.tsx'
+
+initGoogleAnalytics(import.meta.env.VITE_GA_MEASUREMENT_ID)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
