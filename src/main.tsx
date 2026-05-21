36d84e333e2ed1ce0bc8e4604bb716bdaf411eac
ft4k696bk6-prog <ft4k696bk6@privaterelay.appleid.com>
2026-05-21 20:12:39 +0200

    Use Google tag ID for GA4 loader

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { initGoogleAnalytics } from './lib/google-analytics'
 import './index.css'
 import App from './App.tsx'
 
-initGoogleAnalytics(import.meta.env.VITE_GA_MEASUREMENT_ID)
+initGoogleAnalytics(import.meta.env.VITE_GA_MEASUREMENT_ID, import.meta.env.VITE_GOOGLE_TAG_ID)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
